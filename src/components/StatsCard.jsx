12e58f20fd7884319214020dc01702eadff2f9fa
g-akca <gok.akca66@gmail.com>
2026-03-22 05:23:13 +0300

    Add desktop responsive styles

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -3,7 +3,7 @@ import StatsDetails from "./StatsDetails";
 
 function StatsCard() {
   return (
-    <section className="w-full rounded-lg overflow-hidden flex flex-col max-w-141">
+    <section className="w-full rounded-lg overflow-hidden flex flex-col max-w-100 tablet:max-w-141 desktop:grid desktop:grid-cols-2 desktop:max-w-282">
       <StatsImg />
       <StatsDetails />
     </section>
--- src/components/StatsImg.jsx
+++ src/components/StatsImg.jsx
@@ -2,8 +2,8 @@ import mobileImg from "/images/image-header-mobile.jpg";
 
 function StatsImg() {
   return (
-    <div className="bg-purple-500">
-      <img src={mobileImg} className="opacity-75 mix-blend-multiply w-full" />
+    <div className="bg-purple-500 desktop:order-2">
+      <img src={mobileImg} className="opacity-75 mix-blend-multiply w-full h-full desktop:object-cover" />
     </div>
   );
 }
